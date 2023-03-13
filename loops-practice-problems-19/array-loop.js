fad01a2cbdd91cb49cfa5c7f022fccbebdecf431
mdsumonali372 <mdsumonali026@gmail.com>
2023-03-13 23:36:40 +0600

    loop practice

---
+++ loops-practice-problems-19/array-loop.js
@@ -0,0 +1,25 @@
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// target: display every elements of an array
+// var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+
+// for (i = 0; i < 7; i++) {
+//     var number = numbers[i]
+//     console.log(number);
+// }
+
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+
+for (var i = 0; i < items.length; i++) {
+    var item = items[i]
+    console.log(item)
+
+}
---
+++ loops-practice-problems-19/while.js
@@ -0,0 +1,32 @@
+// if(condition){
+
+// }
+
+
+// roastGiven = roastGiven + 1;
+// roastGiven += 1;
+/* 
+1. Loop variable
+2. condition inside while
+3. Loop body
+4. Change the loop variable
+*/
+
+// var roastGiven = 0;
+// while (roastGiven < 10) {
+//     console.log('Roast Den, Please !!')
+//     console.log(roastGiven);
+//     roastGiven++
+// }
+
+
+
+
+
+
+var codingAmonDiteHobe = 0;
+while (codingAmonDiteHobe < 1) {
+    console.log('Coding A Mon Bosbi Kemne Mon To Sadiyar kase🙂')
+    console.log(codingAmonDiteHobe);
+    roastGiven + 2
+}
