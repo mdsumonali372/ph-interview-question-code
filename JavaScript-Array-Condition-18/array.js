fd87ad58e9829455b2726fadd753ad747ed16a11
mdsumonali372 <mdsumonali026@gmail.com>
2023-03-13 23:32:33 +0600

    array condition add

---
+++ JavaScript-Array-Condition-18/array.js
@@ -0,0 +1,16 @@
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+console.log(friendsAge);
+var picnicFee = [5000, 2000, 4000, 150];
+
+var nayikas = ['mahi', "opu", 'sabnoor', 'sabana'];
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");
+
+console.log(fruits);
+
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log()
+
+
+// console.log(friendsAge.length);
---
+++ JavaScript-Array-Condition-18/comparison.js
@@ -0,0 +1,45 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(5 != 6);
+// console.log(15 != 15);
+// less than or equal
+// console.log(5 <= 6)
+// console.log( 6 <= 6 )
+// console.log( 16 <= 6 )
+
+// greater than or equal
+// console.log(5 >= 6)
+// console.log(5 >= 2)
+// console.log(2 >= 2)
+
+var num1 = 56;
+var num2 = 89;
+
+// console.log(num1 < num2);
+// console.log(num1 > num2);
+// console.log(num1 === num2);
+// console.log(num1 !== num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+
+// multiple conditions
+// money1 > money2 && result1 > result2 && height1 > height2
+// money1 > money2 || result1 > result2 || height1 > height2 
+
+/* 
+
+- Introduction to JavaScript comparison operators
+To compare two values, you use a comparison operator. The following table shows the comparison operators in JavaScript:
+
+- Operator	Meaning
+- <	less than
+- >	greater than
+- <=	less than or equal to
+- >=	greater than or equal to
+- ==	equal to
+!=	not equal to   
+
+
+*/
